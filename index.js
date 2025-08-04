11bba539b46a110d6371aebe75f50b8147258245
Matt Elston <m.elston@ufl.edu>
2025-08-03 17:00:47 -0700

    Completed

--- index.js
+++ index.js
@@ -9,7 +9,43 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (poemData) => {
+  const poem = poemData[0];
+  const { title, author, lines } = poem;
+  const makeH2 = makeTag('h2');
+  const makeH3 = makeTag('h3');
+  const makeEm = makeTag('em');
+  const makeP = makeTag('p');
+  const authorLine = pipe(
+    name =>`by ${name}`,
+    makeEm,
+    makeH3
+  )(author);
+
+const stanzas = lines.reduce((acc, line) => {
+  if (line === "") {
+    acc.push([]);
+  } else {
+    if (acc.length === 0) acc.push([]);
+    acc[acc.length - 1].push(line);
+  }
+  return acc;
+},[]);
+
+const stanzaHTML = stanzas.map(stanza =>
+  makeP(
+    stanza.map((line, i) =>
+      i === stanza.length - 1 ? line : `${line}<br>`
+    ).join('')
+  )
+).join('');
+
+return `${makeH2(title)}${authorLine}${stanzaHTML}`;
+};
+
+
+
+
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
